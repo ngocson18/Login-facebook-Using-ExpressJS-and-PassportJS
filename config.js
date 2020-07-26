9a0ff5414c37201560caf3454e5d5ef76d3b6241
ngocson98 <ngocsonxx98@gmail.com>
2020-07-26 13:51:29 +0700

    update code

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'facebookAuth': {
+		'clientID':  '281897193097139', // your App ID
+		'clientSecret':  '4a2a1338cf91ca7a2cbd94a444525cc9', // your App Secret
+		'callbackURL':  'http://localhost:3000/auth/facebook/callback'
+	}
+}
